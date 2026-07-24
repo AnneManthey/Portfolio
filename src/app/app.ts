e8b30c3baaab24f9c4daa97396ce9832a2fcf9c1
Anne Manthey <anne.manthey@pm.me>
2026-07-24 15:38:20 +0200

    feat(skills): add skills component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
 import { Hero } from './pages/content/hero/hero';
 import { AboutMe } from './pages/content/about-me/about-me';
+import { Skills } from './pages/content/skills/skills';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero, AboutMe],
+  imports: [RouterOutlet, Header, Hero, AboutMe, Skills],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
