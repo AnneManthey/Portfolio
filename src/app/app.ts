3d5dbd1c2e1871887a7833f5de2ab950bf97dc04
Anne Manthey <anne.manthey@pm.me>
2026-07-24 15:56:56 +0200

    feat(contact): add contact component

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,10 +6,11 @@ import { AboutMe } from './pages/content/about-me/about-me';
 import { Skills } from './pages/content/skills/skills';
 import { Projects } from './pages/content/projects/projects';
 import { References } from './pages/content/references/references';
+import { Contact } from './pages/content/contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero, AboutMe, Skills, Projects, References],
+  imports: [RouterOutlet, Header, Hero, AboutMe, Skills, Projects, References, Contact],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
