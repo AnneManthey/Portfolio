1efca034017ea48e6462942bab92f4421ab61ca4
Anne Manthey <anne.manthey@pm.me>
2026-07-27 11:29:37 +0200

    feat(skills): render skills from array

--- src/app/pages/content/skills/skills.ts
+++ src/app/pages/content/skills/skills.ts
@@ -6,4 +6,39 @@ import { Component } from '@angular/core';
   templateUrl: './skills.html',
   styleUrl: './skills.scss',
 })
-export class Skills {}
+export class Skills {
+
+  skillList = [
+    {
+      src: "/assets/icons/pages/skills/HTML.png",
+      name:"HTML"
+    },
+    {
+      src: "/assets/icons/pages/skills/CSS.png",
+      name:"CSS"
+    },
+    {
+      src: "/assets/icons/pages/skills/JavaScript.png",
+      name:"JavaScript"
+    },
+    {
+      src: "/assets/icons/pages/skills/TypeScript.png",
+      name:"TypeScript"
+    },
+    {
+      src: "/assets/icons/pages/skills/Angular.png",
+      name:"Angular"
+    },
+    {
+      src: "/assets/icons/pages/skills/Supabase.png",
+      name:"Supabase"
+    },
+    {
+      src: "/assets/icons/pages/skills/Git.png",
+      name:"Git"
+    },
+
+
+  ]
+
+}
