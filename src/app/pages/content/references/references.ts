c8fd641220856c721853269364c78cddb31c9cda
Anne Manthey <anne.manthey@pm.me>
2026-07-28 08:23:58 +0200

    feat(references): slider logic

--- src/app/pages/content/references/references.ts
+++ src/app/pages/content/references/references.ts
@@ -6,4 +6,33 @@ import { Component } from '@angular/core';
   templateUrl: './references.html',
   styleUrl: './references.scss',
 })
-export class References {}
+export class References {
+
+  currentIndex = 0;
+
+  refer = [{
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti recusandae libero saepe soluta dignissimos. Quisquam cumque delectus, laborum at ex nisi eos, iure necessitatibus quasi accusantium asperiores consequuntur odio praesentium.",
+    name: "Jane Dow"
+  },
+{
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti recusandae libero saepe soluta dignissimos. Quisquam cumque delectus, laborum at ex nisi eos, iure necessitatibus quasi accusantium asperiores consequuntur odio praesentium.",
+    name: "Max Musterman"
+  },
+  {
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti recusandae libero saepe soluta dignissimos. Quisquam cumque delectus, laborum at ex nisi eos, iure necessitatibus quasi accusantium asperiores consequuntur odio praesentium.",
+    name: "Shallan Kholin"
+  },
+];
+
+next() {
+    this.currentIndex = (this.currentIndex + 1) % this.refer.length;
+  }
+
+  prev() {
+    this.currentIndex = (this.currentIndex - 1 + this.refer.length) % this.refer.length;
+  }
+
+  goTo(index: number) {
+    this.currentIndex = index;
+  }
+}
