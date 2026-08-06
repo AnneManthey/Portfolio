f41f89dc0ea48c3b66457f67a795d91daa453954
Anne Manthey <anne.manthey@pm.me>
2026-08-06 07:28:28 +0200

    feat(i18n): implement in component (legal & privacy)

--- src/app/pages/legal-notice/legal-notice.ts
+++ src/app/pages/legal-notice/legal-notice.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {
+    TranslateService,
+    TranslatePipe,
+    TranslateDirective
+} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [RouterLink],
+  imports: [RouterLink, TranslatePipe],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss',
 })
-export class LegalNotice {}
+export class LegalNotice {
+  private translate = inject(TranslateService);
+}
--- src/app/pages/privacy-policy/privacy-policy.ts
+++ src/app/pages/privacy-policy/privacy-policy.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {
+    TranslateService,
+    TranslatePipe,
+    TranslateDirective
+} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [RouterLink],
+  imports: [RouterLink, TranslatePipe],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.scss',
 })
-export class PrivacyPolicy {}
+export class PrivacyPolicy {
+  private translate = inject(TranslateService);
+}
